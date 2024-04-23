5ef218953a051d24602b1469d9158c7e45b4e4fc
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-04-23 19:28:59 +0530

    additional lines of code removed

--- expensetracker/src/components/Dashboard.jsx
+++ expensetracker/src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import ExpenseTracker from "./ExpenseTracker";
 import RecentTransaction from "./RecentTransaction";
 import TopExpenses from "./TopExpenses";
 import styles from "./Dashboard.module.css";
-import { useSnackbar } from 'notistack'
+import { useSnackbar } from "notistack";
 
 export const ExpenseListStore = createContext({
   expenseList: [],
@@ -14,7 +14,7 @@ export const ExpenseListStore = createContext({
   expensesData: () => {},
   handleDelete: () => {},
   updateTransaction: () => {},
-  addIncomeBalance: () => {}
+  addIncomeBalance: () => {},
 });
 
 const Dashboard = () => {
@@ -23,14 +23,11 @@ const Dashboard = () => {
   const [invokeState, setInvokeState] = useState(true);
 
   const [showWallet, setShowWallet] = useState(0);
-    const [showExpenses, setShowExpenses] = useState(0);
+  const [showExpenses, setShowExpenses] = useState(0);
 
-    const [getTransactions, setTransactions] = useState(true);
+  const [getTransactions, setTransactions] = useState(true);
 
-
-    const {enqueueSnackbar, closeSnackbar} = useSnackbar();
-
-
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
   useEffect(() => {
     let expenseTracker = localStorage.getItem("expenseTracker");
@@ -40,10 +37,7 @@ const Dashboard = () => {
         totalExpense: 0,
       };
       localStorage.setItem("expenseTracker", JSON.stringify(expenses));
-      
     }
-    
-    
 
     let expensesListInLocal = localStorage.getItem("expenseListFromLocal");
     if (!expensesListInLocal) {
@@ -54,24 +48,20 @@ const Dashboard = () => {
     }
   }, [invokeState]);
 
-//wallet and expense shower
+  //wallet and expense shower
   useEffect(() => {
-    const wallet = localStorage.getItem('expenseTracker');
-    if(wallet){
-        const expenseObj = JSON.parse(wallet);
-        if(isExpenseOnTrack(expenseObj.wallet, "CHECK_EQUAL")){
-          setShowWallet(expenseObj.wallet);
-          setShowExpenses(expenseObj.totalExpense);
-        }else{
-          setShowWallet(expenseObj.wallet);
-          setShowExpenses(expenseObj.totalExpense);
-        }
+    const wallet = localStorage.getItem("expenseTracker");
+    if (wallet) {
+      const expenseObj = JSON.parse(wallet);
+      if (isExpenseOnTrack(expenseObj.wallet, "CHECK_EQUAL")) {
+        setShowWallet(expenseObj.wallet);
+        setShowExpenses(expenseObj.totalExpense);
+      } else {
+        setShowWallet(expenseObj.wallet);
+        setShowExpenses(expenseObj.totalExpense);
+      }
     }
-    
-}, [getTransactions])
-
-
-
+  }, [getTransactions]);
 
   const expensesData = () => {
     setInvokeState(!invokeState);
@@ -84,153 +74,204 @@ const Dashboard = () => {
     };
     localStorage.setItem("expenseTracker", JSON.stringify(toDefault));
     localStorage.setItem("expenseListFromLocal", JSON.stringify([]));
-    setTransactions(!getTransactions)
+    setTransactions(!getTransactions);
     setInvokeState(!invokeState);
-    enqueueSnackbar("Expense tracker reset successful", {variant: "success", anchorOrigin: {horizontal: "center", vertical: "top" }})
+    enqueueSnackbar("Expense tracker reset successful", {
+      variant: "success",
+      anchorOrigin: { horizontal: "center", vertical: "top" },
+    });
   };
 
-  const addTransaction = ({title, price, category, date, id}) => {
-
-    
-    if(isExpenseOnTrack(price, "CHECK_ADD")){
+  const addTransaction = ({ title, price, category, date, id }) => {
+    if (isExpenseOnTrack(price, "CHECK_ADD")) {
       let expenseTrackerObj = {};
-    let latest = JSON.parse(localStorage.getItem('expenseTracker'));
-      expenseTrackerObj.wallet = latest.wallet - price
-    expenseTrackerObj.totalExpense = latest.totalExpense + price
-      localStorage.setItem('expenseListFromLocal', JSON.stringify([{title, price, category, date, id}, ...JSON.parse(localStorage.getItem('expenseListFromLocal'))]));
-      localStorage.setItem('expenseTracker', JSON.stringify(expenseTrackerObj));
-      setTransactions(!getTransactions)
-      expensesData()
-      enqueueSnackbar("Added to your Expense List", {variant: "success", anchorOrigin: {horizontal: "center", vertical: "top" }})
+      let latest = JSON.parse(localStorage.getItem("expenseTracker"));
+      expenseTrackerObj.wallet = latest.wallet - price;
+      expenseTrackerObj.totalExpense = latest.totalExpense + price;
+      localStorage.setItem(
+        "expenseListFromLocal",
+        JSON.stringify([
+          { title, price, category, date, id },
+          ...JSON.parse(localStorage.getItem("expenseListFromLocal")),
+        ])
+      );
+      localStorage.setItem("expenseTracker", JSON.stringify(expenseTrackerObj));
+      setTransactions(!getTransactions);
+      expensesData();
+      enqueueSnackbar("Added to your Expense List", {
+        variant: "success",
+        anchorOrigin: { horizontal: "center", vertical: "top" },
+      });
+    }
+  };
+
+  const handleDelete = (id, price) => {
+    let expenseTrackerObj = {};
+    let latest = JSON.parse(localStorage.getItem("expenseTracker"));
+
+    expenseTrackerObj.wallet = latest.wallet + price;
+    expenseTrackerObj.totalExpense = latest.totalExpense - price;
+
+    localStorage.setItem("expenseTracker", JSON.stringify(expenseTrackerObj));
+    const deletedList = expenseList.filter((x) => x.id != id);
+    localStorage.setItem("expenseListFromLocal", JSON.stringify(deletedList));
+
+    setTransactions(!getTransactions);
+    expensesData();
+    enqueueSnackbar("Deleted from your Expense List", {
+      variant: "error",
+      anchorOrigin: { horizontal: "center", vertical: "top" },
+    });
+  };
+
+  const updateTransaction = ({
+    title,
+    price,
+    category,
+    date,
+    id,
+    prevPrice,
+  }) => {
+    const index = expenseList.findIndex((x) => x.id === id);
+    const updateItem = expenseList.splice(index, 1, {
+      title,
+      price,
+      category,
+      date,
+      id,
+    });
+    localStorage.setItem("expenseListFromLocal", JSON.stringify(expenseList));
+    let expenseTrackerObj = {};
+    let latest = JSON.parse(localStorage.getItem("expenseTracker"));
+    expenseTrackerObj.wallet = latest.wallet + updateItem[0].price - price;
+    expenseTrackerObj.totalExpense =
+      latest.totalExpense - updateItem[0].price + price;
+
+    localStorage.setItem("expenseTracker", JSON.stringify(expenseTrackerObj));
+
+    setTransactions(!getTransactions);
+    expensesData();
+    enqueueSnackbar("Your Expense List was edited", {
+      variant: "success",
+      anchorOrigin: { horizontal: "center", vertical: "top" },
+    });
+  };
+
+  const addIncomeBalance = (val) => {
+    let latest = JSON.parse(localStorage.getItem("expenseTracker"));
+    if (val > 0) {
+      latest.wallet += Number(val);
+    } else {
+      enqueueSnackbar("Wallet balance should be more than 0", {
+        variant: "success",
+        anchorOrigin: { horizontal: "center", vertical: "top" },
+      });
     }
 
-    
-  } 
+    localStorage.setItem("expenseTracker", JSON.stringify(latest));
+    setTransactions(!getTransactions);
+    expensesData();
+    enqueueSnackbar("Your balance has been updated", {
+      variant: "success",
+      anchorOrigin: { horizontal: "center", vertical: "top" },
+    });
+  };
 
-
-
-
-    const handleDelete = (id, price) => {
-
-      let expenseTrackerObj = {};
-        let latest = JSON.parse(localStorage.getItem('expenseTracker'));
-
-        
-        expenseTrackerObj.wallet = latest.wallet + price
-        expenseTrackerObj.totalExpense = latest.totalExpense - price
-
-        
-        localStorage.setItem('expenseTracker', JSON.stringify(expenseTrackerObj));
-        const deletedList = expenseList.filter(x => x.id != id);
-        localStorage.setItem('expenseListFromLocal', JSON.stringify(deletedList))
-  
-  
-        
-          setTransactions(!getTransactions)
-          expensesData()
-          enqueueSnackbar("Deleted from your Expense List", {variant: "error", anchorOrigin: {horizontal: "center", vertical: "top" }})
-        
-    }
-
-
-
-
-    const updateTransaction = ({title, price, category, date, id, prevPrice}) => {
-        const index = expenseList.findIndex(x => x.id === id);
-        const updateItem = expenseList.splice(index, 1, {title, price, category, date, id})
-        localStorage.setItem('expenseListFromLocal', JSON.stringify(expenseList))
-        let expenseTrackerObj = {};
-          let latest = JSON.parse(localStorage.getItem('expenseTracker'));
-          expenseTrackerObj.wallet = latest.wallet + updateItem[0].price - price;
-          expenseTrackerObj.totalExpense = latest.totalExpense - updateItem[0].price + price;
-          
-          localStorage.setItem('expenseTracker', JSON.stringify(expenseTrackerObj));
-
-          
-            setTransactions(!getTransactions)
-            expensesData()
-            enqueueSnackbar("Your Expense List was edited", {variant: "success", anchorOrigin: {horizontal: "center", vertical: "top" }})
-
-    }
-
-    const addIncomeBalance = (val) => {
-        let latest = JSON.parse(localStorage.getItem('expenseTracker'));
-        if(val > 0){
-            latest.wallet+=Number(val);
-        }else{
-          enqueueSnackbar("Wallet balance should be more than 0", {variant: "success", anchorOrigin: {horizontal: "center", vertical: "top" }})
+  const isExpenseOnTrack = (newTotalExpense, action) => {
+    let latestEpenseTracker = JSON.parse(
+      localStorage.getItem("expenseTracker")
+    );
+    let { wallet, totalExpense } = latestEpenseTracker;
+    switch (action) {
+      case "CHECK_ADD":
+        if (newTotalExpense > wallet) {
+          enqueueSnackbar("Your wallet balance is low", {
+            variant: "warning",
+            anchorOrigin: { horizontal: "center", vertical: "top" },
+          });
+          return false;
         }
-
-        localStorage.setItem('expenseTracker', JSON.stringify(latest));
-        setTransactions(!getTransactions)
-        expensesData()
-        enqueueSnackbar("Your balance has been updated", {variant: "success", anchorOrigin: {horizontal: "center", vertical: "top" }})
-    }
-
-
-    const isExpenseOnTrack = (newTotalExpense, action) => {
-      let latestEpenseTracker = JSON.parse(localStorage.getItem('expenseTracker'));
-      let {wallet, totalExpense} = latestEpenseTracker;
-        switch(action){
-          case 'CHECK_ADD':
-            if(newTotalExpense > wallet){
-              enqueueSnackbar("Your wallet balance is low", {variant: "warning", anchorOrigin: {horizontal: "center", vertical: "top" }})
-              return false;
-            }
-          case "CHECK_EQUAL":
-            if(newTotalExpense <= 0){
-              enqueueSnackbar("Your wallet balance is 0, Update your wallet.", {variant: "error", anchorOrigin: {horizontal: "center", vertical: "bottom" }})
-              return false;
-            }
-          default:
-            return true
+      case "CHECK_EQUAL":
+        if (newTotalExpense <= 0) {
+          enqueueSnackbar("Your wallet balance is 0, Update your wallet.", {
+            variant: "error",
+            anchorOrigin: { horizontal: "center", vertical: "bottom" },
+          });
+          return false;
         }
-      
+      default:
+        return true;
     }
-  
+  };
+
   return (
     <>
-    <ExpenseListStore.Provider value={{ expenseList, handleReset, addTransaction, showWallet, showExpenses,  expensesData, handleDelete, updateTransaction, addIncomeBalance }}>
-      <div className={styles.mainContainer}>
-        <div style={{ display: "flex", width: "100%" }}>
-          <h1 style={{ textAlign: "left", color: "var(--text-white)" }}>
-            Expense Tracker
-          </h1>
-        </div>
-        <div className={styles.container1}>
-          <ExpenseTracker
-            expensesData={expensesData}
-          />
-        </div>
-        <div className={styles.container2}>
-          <div className={styles.transactionsContainer} >
-          <div style={{ display: "flex", width: "100%",  marginBottom: "1rem", justifyContent: "center", alignItems: "center"}}>
-            <h1 style={{ textAlign: "left, ", color: "var(--text-white)" }}>
-              Recent Transactions
-            </h1>
-            <button className={styles.resetBtn} onClick={() => handleReset()}>Reset</button>
-          </div>
-          <div className={styles.subContainer1}>
-            <RecentTransaction expensesData={expensesData}/>
-          </div>
-          </div>
-
-          <div className={styles.topExpensesContainer} style={{marginBottom: "3rem"}}>
-          <div style={{ display: "flex", width: "100%",  marginBottom: "1rem"}}>
+      <ExpenseListStore.Provider
+        value={{
+          expenseList,
+          handleReset,
+          addTransaction,
+          showWallet,
+          showExpenses,
+          expensesData,
+          handleDelete,
+          updateTransaction,
+          addIncomeBalance,
+        }}
+      >
+        <div className={styles.mainContainer}>
+          <div style={{ display: "flex", width: "100%" }}>
             <h1 style={{ textAlign: "left", color: "var(--text-white)" }}>
-              Top Expenses
+              Expense Tracker
             </h1>
           </div>
-          <div className={styles.subContainer2}>
-            <TopExpenses />
+          <div className={styles.container1}>
+            <ExpenseTracker />
           </div>
+          <div className={styles.container2}>
+            <div className={styles.transactionsContainer}>
+              <div
+                style={{
+                  display: "flex",
+                  width: "100%",
+                  marginBottom: "1rem",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <h1 style={{ textAlign: "left, ", color: "var(--text-white)" }}>
+                  Recent Transactions
+                </h1>
+                <button
+                  className={styles.resetBtn}
+                  onClick={() => handleReset()}
+                >
+                  Reset
+                </button>
+              </div>
+              <div className={styles.subContainer1}>
+                <RecentTransaction />
+              </div>
+            </div>
+
+            <div
+              className={styles.topExpensesContainer}
+              style={{ marginBottom: "3rem" }}
+            >
+              <div
+                style={{ display: "flex", width: "100%", marginBottom: "1rem" }}
+              >
+                <h1 style={{ textAlign: "left", color: "var(--text-white)" }}>
+                  Top Expenses
+                </h1>
+              </div>
+              <div className={styles.subContainer2}>
+                <TopExpenses />
+              </div>
+            </div>
           </div>
         </div>
-        
-      </div>
-      
-    </ExpenseListStore.Provider>
-    
+      </ExpenseListStore.Provider>
     </>
   );
 };
